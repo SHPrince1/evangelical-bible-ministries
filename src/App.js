80a007475cc27eeba394b8edea557f88fc8df8f0
Sylvanus Prince <princehsylvanus@gmail.com>
2022-10-02 22:03:30 +0100

    browser changed to hash

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter as Router, Routes, Route} from 'react-router-dom';
+import { HashRouter as Router, Routes, Route} from 'react-router-dom';
 import './App.css';
 import Home from './pages';
 import About from './pages/about';
